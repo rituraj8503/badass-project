9670198767544f260536f920c907696b97b947c3
vianarafael <vianarafaelds@gmail.com>
2021-07-21 11:21:17 +0900

    fist

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/dist"));
+
+app.use("/api", (req, res) => {
+  res.send("olá, mundo 🌎!!!");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("App listening on port " + port);
+});
